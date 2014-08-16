4ddb6f46cc14cf142864ff1cf59eed8e014d41f1
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2014-08-15 18:34:29 -0700

    working on merge

--- logger.cpp
+++ logger.cpp
@@ -22,23 +22,75 @@
 #include <fstream>
 #include <sstream>
 #include "SwansonLibs/swansonInput.hpp"
+//#include "SwansonObjects/menu.hpp"
 
 using namespace std;
 
-void demonstrateComponent();
 
+
+class Building{
+private:
+
+   class Occupant{
+   private:
+      string name;
+   public:
+      Occupant(string nameCons):name(nameCons){}
+   };
+
+   vector<Occupant> occupants;
+   typedef vector<Occupant>::iterator logBook;
+   int NumOcupants;
+
+public:
+   Building():NumOcupants(0){}
+
+   bool IsInBuilding(string name);
+   bool RemoveFromBuilding(string name);
+   bool EnterBuilding(string name);
+
+
+};
+
+bool Building::IsInBuilding(string name){
+   for(logBook lb = occupants.begin(); lb != occupants.end(); lb++){
+      if(name == *lb) return true;
+   }
+   return false;
+}
+bool Building::RemoveFromBuilding(string name){
+   if(!IsInBuilding(name)) return false;
+   for(logBook lb = occupants.begin(); lb != occupants.end(); lb++){
+      if(name == *lb){
+         occupants.erase(lb);
+         return true;
+      }
+   }
+
+}
+bool Building::EnterBuilding(string name){
+
+   //to do insert
+
+}
 
 /////////////////////////////////////////////////
 ////////////Introduction and repetition ////////
 ////////////////////////////////////////////////
 int main(){
 
-   string intro = "";
+   struct MenuItem{
+      int number;
+      string title;
+   };
+
+   string intro = "This will log the Ins and Outs of individuals in a building";
 
    do{
 
-     cout << intro;
-     demonstrateComponent();
+     cout << intro << endl;
+
+
 
    } while ( swansonInput::yesNo( "Would you like to see that again" ) );
 
@@ -51,10 +103,7 @@ int main(){
 ////////////Exercise Component /////////////////
 ////////////////////////////////////////////////
 
-void demonstrateComponent(){
 
-
-}
 
 
 
