11b644640d7937ee61da2ef4caa15ed0d20b10e5
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2014-08-15 21:34:13 -0700

    3 exercises down

--- getInt.cpp
+++ getInt.cpp
@@ -22,10 +22,49 @@
 #include <fstream>
 #include <sstream>
 #include "SwansonLibs/swansonInput.hpp"
+#include <climits>
 
 using namespace std;
 
 void demonstrateComponent();
+
+
+
+/******************************************************************************
+ *    purpose: receive a value from the user, screen for alpha input
+ *             and overflow, restrict input to specific range
+ *
+ *    entry: a string to be used to clarify desired input to user
+ *
+ *    exit: a value between rangeMin and rangeMax
+ ******************************************************************************/
+
+int GetInt ( string prompt , int rangeMin=INT_MIN , int rangeMax = INT_MAX) {
+   int int_rtrn;
+   string parse_string;
+   bool firstTimeThrough = true;
+
+   do {
+      if ( !firstTimeThrough ) {
+         cout << "Please keep the input within these bounds [" << rangeMin
+               << " - " << rangeMax << "]";
+      }
+      firstTimeThrough = false;
+      parse_string = swansonInput::GetString( prompt );
+
+      while ( !swansonString::AllNumbers( parse_string ) || parse_string.empty() ) {
+
+         cout << "lets try to restrain ourselves to only whole numbers";
+         parse_string = swansonInput::GetString( prompt );
+      }
+
+      stringstream extract(parse_string);
+      extract >> int_rtrn;
+
+   } while ( !(int_rtrn >= rangeMin && int_rtrn <= rangeMax) );
+
+   return int_rtrn;
+}
 
 
 /////////////////////////////////////////////////
@@ -40,7 +79,7 @@ int main(){
      cout << intro;
      demonstrateComponent();
 
-   } while ( swansonInput::yesNo( "Would you like to see that again" ) );
+   } while ( swansonInput::yesNo( "Would you like to enter another number" ) );
 
    return 0;
 
@@ -53,6 +92,10 @@ int main(){
 
 void demonstrateComponent(){
 
+   int inptNum = GetInt("What is the number [-1000,1000]:",-1000,1000);
+   cout << "the number is " << inptNum
+         << " and as a hex " << std::hex << inptNum
+         << std::dec << endl;
 
 }
 
--- logger.cpp
+++ logger.cpp
@@ -21,90 +21,202 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
+#include <vector>
 #include "SwansonLibs/swansonInput.hpp"
 //#include "SwansonObjects/menu.hpp"
 
 using namespace std;
 
-
-
-class Building{
+class Building {
 private:
 
-   class Occupant{
-   private:
-      string name;
+   class Occupant {
    public:
-      Occupant(string nameCons):name(nameCons){}
+      string name;
+      Occupant ( string nameCons ) :
+            name( nameCons ) {
+      }
    };
 
-   vector<Occupant> occupants;
-   typedef vector<Occupant>::iterator logBook;
-   int NumOcupants;
+   vector<Building::Occupant> occupants;
+   typedef vector<Building::Occupant>::iterator logBook;
 
 public:
-   Building():NumOcupants(0){}
 
-   bool IsInBuilding(string name);
-   bool RemoveFromBuilding(string name);
-   bool EnterBuilding(string name);
+   bool IsInBuilding ( string name );
+   bool RemoveFromBuilding ( string name );
+   bool EnterBuilding ( string name );
+   int NumberOfOcupants ();
+   string GetNamebyNumber ( int logBookNumber ) {
+      return occupants.at( logBookNumber - 1 ).name;
+   }
 
+   friend ostream& operator << ( ostream& out , Building& bld );
 
 };
 
-bool Building::IsInBuilding(string name){
-   for(logBook lb = occupants.begin(); lb != occupants.end(); lb++){
-      if(name == *lb) return true;
+//true if occupant of that name is in building
+bool Building::IsInBuilding ( string name ) {
+   for ( logBook lb = occupants.begin() ; lb != occupants.end() ; lb++ ) {
+      if ( name == lb->name )
+         return true;
    }
    return false;
 }
-bool Building::RemoveFromBuilding(string name){
-   if(!IsInBuilding(name)) return false;
-   for(logBook lb = occupants.begin(); lb != occupants.end(); lb++){
-      if(name == *lb){
-         occupants.erase(lb);
-         return true;
+
+//removes occupant, returns false if person of that name is not in building
+bool Building::RemoveFromBuilding ( string name ) {
+   if ( !IsInBuilding( name ) )
+      return false;
+   for ( logBook lb = occupants.begin() ; lb != occupants.end() ; lb++ ) {
+      if ( name == lb->name ) {
+         occupants.erase( lb );
+         break;
       }
    }
+   return true;
 
 }
-bool Building::EnterBuilding(string name){
 
-   //to do insert
+//inserts new Ocupant, returns false if person of that name is in building
+bool Building::EnterBuilding ( string name ) {
 
+   if ( IsInBuilding( name ) )
+      return false;
+   this->occupants.push_back( Occupant( name ) );
+   return true;
+
+}
+
+//return the number of people in the building
+int Building::NumberOfOcupants () {
+   return this->occupants.size();
+}
+
+//this function could also be built using the public methods,
+//num tenets and tenet name by number
+ostream& operator << ( ostream& out , Building& bld ) {
+
+   //ios::fmtflags flagSettings = out.flags();
+   //todo try collumn output
+
+   out <<
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+         << endl <<
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~BUILDING OCUPANTS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+         << endl <<
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+         << endl;
+
+   for ( int i = 0 ; i < bld.occupants.size() ; i++ ) {
+      out << i + 1 << ": " << bld.occupants.at( i ).name << endl;
+   }
+
+   //out.flags(flagSettings);
+
+   out <<
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            << endl;
+
+   return out;
 }
 
 /////////////////////////////////////////////////
 ////////////Introduction and repetition ////////
 ////////////////////////////////////////////////
-int main(){
+struct BldOption {
+   int number;
+   string title;
+};
 
-   struct MenuItem{
-      int number;
-      string title;
-   };
+int displayMenu ( BldOption* options , int numOptions ) {
+   for ( int i = 0 ; i < numOptions ; i++ ) {
+      cout << options[i].number << ") " << options[i].title << endl;
+   }
+   return swansonInput::GetInt( "chose an option:" , 1 , numOptions );
+}
+
+int main () {
+
+   const BldOption bldIN = { 1, "Admit a person to the building" };
+   const BldOption bldOUT = { 2,
+         "Log the exit of a person from the building (by Name)" };
+   const BldOption bldOUTnm = { 3,
+         "Log the exit of a person from the building (by Number)" };
+   const BldOption bldDISP = { 4, "Show the occupants of the building" };
+   const BldOption bldQUIT = { 5, "Quit Program" };
+
+   int optionNum = 5;
+   BldOption options[] = { bldIN, bldOUT, bldOUTnm, bldDISP, bldQUIT };
 
    string intro = "This will log the Ins and Outs of individuals in a building";
 
-   do{
+   cout << intro << endl;
 
-     cout << intro << endl;
+   bool exit = false;
+   Building myBuilding;
+
+   do {
+
+      int selection = displayMenu( options , optionNum );
+
+      if ( selection == bldIN.number ) {
+         string Inname = swansonInput::GetString(
+               "What is the name of the new person: " );
+         if ( myBuilding.EnterBuilding( Inname ) ) {
+            cout << Inname << " added to building logbook" << endl;
+         } else {
+            cout << "there is already a person of that name in the building"
+                  << "\n(this building has weird rules, no duplicate names)"
+                  << endl;
+         }
+
+      } else if ( selection == bldOUTnm.number ) {
+
+         if(myBuilding.NumberOfOcupants()==0){
+            cout << "the building is empty right now\n" << endl;
+         }else{
+            cout << myBuilding;
+            int lbNum = swansonInput::GetInt(
+                  "Select the person who is leaving:" ,
+                  1 , myBuilding.NumberOfOcupants() );
+            myBuilding.RemoveFromBuilding(
+                  myBuilding.GetNamebyNumber( lbNum ) );
+         }
 
 
 
-   } while ( swansonInput::yesNo( "Would you like to see that again" ) );
+      }
+
+      else if ( selection == bldOUT.number ) {
+         if(myBuilding.NumberOfOcupants()==0){
+           cout << "the building is empty right now\n" << endl;
+         }else{
+            string Outname = swansonInput::GetString(
+                  "What is the name of the leaving person: " );
+            if ( myBuilding.RemoveFromBuilding( Outname ) ) {
+               cout << Outname << " removed from building logbook" << endl;
+            } else {
+               cout << "there no person of that name in the building" << endl;
+            }
+         }
+
+      } else if ( selection == bldDISP.number ) {
+
+         cout << myBuilding;
+
+      } else if ( selection == bldQUIT.number ) {
+         exit = true;
+      }
+
+   } while ( !exit );
 
    return 0;
 
 }
 
-
 /////////////////////////////////////////////////
 ////////////Exercise Component /////////////////
 ////////////////////////////////////////////////
-
-
-
-
-
 
